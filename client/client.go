cb1b40d06230aa8816c6b3356e4bd84bdf54b76c
jahichka <jahicamina02@gmail.com>
2024-06-01 14:01:54 +0200

    added instructions

--- client/client.go
+++ client/client.go
@@ -1,22 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"strings"
 )
 
 func main() {
 	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error connecting:", err)
 		return
 	}
+	defer conn.Close()
 
-	_, err = conn.Write([]byte("Hello, server!"))
-	if err != nil {
-		fmt.Println(err)
-		return
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("Enter command (TIME, ECHO <message>, EXIT): ")
+		command, _ := reader.ReadString('\n')
+		command = strings.TrimSpace(command)
+
+		_, err := conn.Write([]byte(command))
+		if err != nil {
+			fmt.Println("Error sending command:", err)
+			return
+		}
+
+		if strings.HasPrefix(command, "EXIT") {
+			break
+		}
+
+		response, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading response:", err)
+			break
+		}
+
+		fmt.Println("Server response:", response)
+
 	}
-
-	conn.Close()
 }
--- server/server.go
+++ server/server.go
@@ -3,7 +3,42 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
+	"time"
 )
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	buffer := make([]byte, 1024)
+
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading:", err)
+			return
+		}
+
+		command := string(buffer[:n])
+		command = strings.TrimSpace(command)
+
+		var response string
+
+		switch {
+		case strings.HasPrefix(command, "TIME"):
+			response = fmt.Sprintf("Server time: %s", time.Now().Format(time.RFC1123))
+		case strings.HasPrefix(command, "ECHO"):
+			response = "Echo: " + strings.TrimSpace(command[5:])
+		case strings.HasPrefix(command, "EXIT"):
+			response = "Goodbye!"
+			conn.Write([]byte(response))
+			return
+		default:
+			response = "Unknown command"
+		}
+
+		conn.Write([]byte(response))
+	}
+}
 
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
@@ -13,23 +48,13 @@ func main() {
 	}
 
 	defer listener.Close()
+	fmt.Println("Server is listening on port 8080")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error:", err)
+			fmt.Println("Error accepting connection:", err)
 			continue
 		}
-		go handleConnection(conn)
+		go handleClient(conn)
 	}
-}
-
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	buffer := make([]byte, 1024)
-	n, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading:", err)
-		return
-	}
-	fmt.Printf("Received data: %s\n", string(buffer[:n]))
 }
